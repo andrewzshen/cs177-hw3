d1fd3021d2452e9e30ddaa5ec70a519039df0258
Andrew Shen <andrewshenrocks@gmail.com>
2026-05-16 13:05:12 -0700

    Some annotating

--- pgd_attack.py
+++ pgd_attack.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
-import sys
 import io
+import requests
 import torch
 
 from PIL import Image
+from torch import Tensor
 from torchvision import transforms
 
-SERVER_IP = "cs177.seclab.cs.ucsb.edu"
+SERVER_IP: str = "cs177.seclab.cs.ucsb.edu"
+PORT: int = 0
 
-def to_vect(path):
+SAMPLE_IMAGE: str = "image.png"  
+
+def to_vect(path: str) -> Tensor:
     img = Image.open(path).convert("RGB")
     t = transforms.ToTensor()(transforms.Resize(256)(img))
     _, h, w = t.shape
@@ -17,39 +21,24 @@ def to_vect(path):
     clean = t[:, y:y+224, x:x+224].clone()
     adv = clean.clone()
 
-def to_png(vect):
+def to_png(vect: Tensor) -> bytes:
     buf = io.BytesIO()
     transforms.ToPILImage()(vect.clamp(0,1)).save(buf, format="PNG")
     return buf.getvalue()
 
-def gradient():
+def predict(img_vect):
+    png = to_png(img_vect)
+
+    r = requests.post(
+        f"https://{SERVER_IP}:{PORT}/api/predict",
+        files={"file": (SAMPLE_IMAGE, png, "image/png")}
+    )
+
+def gradient(im):
     pass 
 
-def predict():
-    pass
-
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python pgd_attack.py <port>")
-        return 1 
-
-    port = int(sys.argv[1])
-
-    packet = IP(dst=SERVER_IP) / UDP(dport=port) / DNS(qd=DNSQR(qname=DOMAIN, qtype=1))
-
-    response = sr1(packet, timeout=3, verbose=0)
-
-    if response:
-        response.show()
-
-        request_size = len(bytes(packet[DNS]))
-        response_size = len(bytes(response[DNS])) if response else 0
-
-        print(f"Request Size: {request_size}")
-        print(f"Response Size: {response_size}")
-        print(f"BAF: {response_size / request_size:.2f}")
-    else:
-        print("No response")
+    pass 
     
 if __name__ == "__main__":
     main()
